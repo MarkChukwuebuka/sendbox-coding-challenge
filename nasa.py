0d9e6274d0cbd282b39003bcdf4e8d11bea95aa3
Mark Chukwuebuka <markedah9652@gmail.com>
2021-08-08 18:39:38 +0100

    added comments

--- nasa.py
+++ nasa.py
@@ -1,11 +1,16 @@
 def move():
+    # user enters coordinates and movements
+    #input format for coordinates: 1 2 N
+    #input format for movements: L M M M L R R
     cordinates = input("enter cordinates: ").split(" ")
     movement = input("enter movement: ").split(" ")
+
     X = int(cordinates[0])
     Y = int(cordinates[1])
     pos = cordinates[2]
     
-    for i in movement:    
+    for i in movement: 
+        # check current letter is L or R
         if i != "M":
             if i == "L":
                 if pos == "W":
@@ -25,6 +30,7 @@ def move():
                     pos="E"
                 else:
                     pos="W"
+        # executes if the letter is M
         else:
             if pos == "W":
                 X -= 1
@@ -35,3 +41,7 @@ def move():
             else:
                 Y -= 1
     return X, Y, pos
+
+
+# function call
+move()
